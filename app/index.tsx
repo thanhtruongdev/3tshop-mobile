aaad80a4374024698c27b1371f85f5a5624fe81c
thanhtruong7103 <=>
2025-10-19 23:39:15 +0700

    feat: update app icon and improve ESLint configuration

--- app/index.tsx
+++ app/index.tsx
@@ -2,5 +2,5 @@ import { Redirect } from "expo-router";
 import "../global.css";
 
 export default function App() {
-  return <Redirect href="/auth/index" />;
+  return <Redirect href="/auth" />;
 }
--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,36 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
+	expoConfig,
+	eslintPluginPrettierRecommended,
+	{
+		ignores: ['dist/*', 'node_modules/*', '.expo/*', '**/build/**', '**/ios/**', '**/android/**', '**/assets/**', '**/fastlane/**'],
+	},
+	{
+		files: ['src/**/*.{js,jsx,ts,tsx}'],
+		rules: {
+			'no-unused-vars': 'warn',
+			'react/prop-types': 'off',
+			'react/react-in-jsx-scope': 'off',
+			'@typescript-eslint/no-explicit-any': 'error',
+			'prettier/prettier': [
+				'warn',
+				{
+					printWidth: 140,
+					tabWidth: 2,
+					useTabs: true,
+					singleQuote: true,
+					semi: true,
+					trailingComma: 'all',
+					bracketSpacing: true,
+					arrowParens: 'always',
+					bracketSameLine: false,
+					endOfLine: 'lf',
+				},
+			],
+		},
+	},
 ]);
